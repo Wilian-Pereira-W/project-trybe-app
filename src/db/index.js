fcd3931513fc7f69d9af87ee24bcd554b66a2984
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-25 20:33:44 -0300

    feat:  mock depoimentos

---
+++ src/db/index.js
@@ -0,0 +1,50 @@
+export const testimony = [
+  {
+    id: 1,
+    imagens: 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9cadd60bc5798_Ellipse%201.png',
+    name: 'Anderson Adriano Godoy',
+    turma: 'Turma 05',
+    hired: 'Contratado pela Accenture',
+    testimony: '"Iniciei minha jornada na Trybe em Abril e logo nos primeiros meses já me senti muito mais confiante, que em anos de faculdade e pós graduação. Com apenas 2 meses de curso, junto a um amigo, fomos capazes de criar um site com informações em tempo real do COVID-19. Desafios e projetos práticos, nos proporcionam uma vivência real do dia a dia de uma pessoa desenvolvedora, atrelado às soft skills. Tenho a plena certeza que venho me tornando uma pessoa melhor!"'
+  },
+  {
+    id: 2,
+    imagens: 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9ca9ce6bc574d_Ellipse%203.png',
+    name: 'Carla Nakajuni',
+    turma: 'Turma 05',
+    hired: 'Contratada pelo C6 Bank',
+    testimony: '"A Trybe acelerou minha carreira de uma forma que eu nunca imaginei ser possível. Não tenho nenhum background em tecnologia e sempre tive dificuldades com lógica de programação. Em apenas 6 meses de curso consegui embarcar no mercado de trabalho, com todos ensinamentos de hard skills e, principalmente, soft skills."'
+  }, 
+  {
+    id: 3,
+    imagens: 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9cab112bc574e_Ellipse%202.png',
+    name: 'Hebert Freitas',
+    turma: 'Turma 03',
+    hired: 'Contratado pela Arco Educação',
+    testimony: '“Sou muito grato por tudo que o ambiente da Trybe me proporciona! Não é fácil, tem dias que eu acho que não vou conseguir, mas sempre pude contar com uma mão que me ajudou a dar o próximo passo. Todo dia um desafio, todo dia um aprendizado, não só para me tornar uma pessoa desenvolvedora, um profissional da área de tecnologia, mas também uma pessoa melhor.”'
+  },
+  {
+    id: 4,
+    imagens: 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9ca2c25bc579f_Ellipse%2033.png',
+    name: 'Renata Novaes',
+    turma: 'Turma 05',
+    hired: 'Contratada pela INCO Investimentos',
+    testimony: '“Estudar na Trybe foi com certeza a melhor decisão que tomei em 2020. Neste ano em que o mundo parou, eu tive a sensação de me reinventar inúmeras vezes e de estar evoluindo para melhor como pessoa e como profissional, diante de todos os desafios e aprendizados que o curso já me proporcionou. Nunca imaginei que conseguiria aprender programação tão rápido e ainda poder desenvolver meu autoconhecimento, conciliando as trilhas de hard skills e soft skills!”'
+  },
+  {
+    id: 5,
+    imagens: 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9ca81efbc5750_Ellipse%206.png',
+    name: 'Lucas Veras',
+    turma: 'Turma 06',
+    hired: 'Contratado pela Stone',
+    testimony: '“A experiência com a Trybe está superando todas as minhas expectativas. O currículo completo e a estrutura do curso, que utiliza as ferramentas do mercado de tecnologia em todo o processo de aprendizagem, fizeram toda a diferença para que eu conseguisse, ainda no final do módulo de front end, uma colocação no mercado como estagiário de desenvolvimento de sistemas.”'
+  },
+  {
+    id: 6,
+    imagens: 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9ca0036bc5751_Ellipse%205.png',
+    name: 'Richard Folgado',
+    turma: 'Turma 03',
+    hired: 'Contratado pela U4Crypto',
+    testimony: '“Se eu pudesse resumir o que a Trybe representa na minha vida em uma palavra, eu diria crescimento. Em todos os aspectos. Minha carreira alavancou, minha vida pessoal alavancou, e eu serei eternamente grato por todas as experiências.”'
+  }
+]
