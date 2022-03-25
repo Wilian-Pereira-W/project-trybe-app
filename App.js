74f19b73f20cb17828abc11d0f200e0439088275
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-25 12:16:56 -0300

    feat: Implementando as rotas

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import { Image, View, TouchableOpacity, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet } from 'react-native';
+import Splash from './src/pages/Splash';
+import Home from './src/pages/Home';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen />
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen 
+          name="Splash"
+          component={Splash}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerStyle: {
+              backgroundColor: '#036B52',
+            },
+            headerTitle: () => (
+                <View style={styles.container}>
+                  <Image style={styles.logo}
+                    source={require('./src/assets/images/logo.png')} />
+                  <View style={styles.menu}>
+                    <TouchableOpacity>
+                      <Text style={styles.login}>Login</Text>
+                    </TouchableOpacity>
+                    <Image style={styles.menuImg}
+                      source={require('./src/assets/images/menu.png')} />
+                  </View>
+                </View>
+            )
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -16,9 +47,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '110%',
+    borderBottomWidth: 7,
+    borderBottomColor: '#2FC18C',
   },
+  menu: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 100,
+    marginLeft: 120
+  },
+  logo : {
+    marginLeft: 50,
+    width: 100,
+    height: 70,
+  },
+  menuImg: {
+    width: 30,
+    height: 30,
+  },
+  login: {
+    color:  '#9ae6ca',
+  }
 });
