8ff572f7cae86d76473ee922d788d234fe7bfb01
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-27 00:18:34 -0300

    feat: implementando CarouselCompany

---
+++ src/components/CarouselCompany/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+function CarouselCompany( {item} ) {
+  return (
+    <View style={styles.container}>
+        <Image style={styles.img}
+          source={item.img}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: 170,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    width: '100%',
+    maxHeight: 400,
+  }
+});
+
+export default CarouselCompany;
