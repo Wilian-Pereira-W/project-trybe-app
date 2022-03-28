f603b978436694ab9bea647bc73f7243ff137ce3
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-28 02:19:21 -0300

    feat: implementando os links no menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Linking} from 'react-native';
 
 function Menu() {
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.containerBtn}>
+      <TouchableOpacity style={styles.containerBtn} onPress={() => Linking.openURL('https://www.betrybe.com/formacao-desenvolvimento-web')}>
         <Text style={styles.containerBtnText}>Formação</Text>
       </TouchableOpacity>
-      <TouchableOpacity  style={styles.containerBtn}>
+      <TouchableOpacity  style={styles.containerBtn} onPress={() => Linking.openURL('https://www.betrybe.com/msc-modelo-de-sucesso-compartilhado')}>
         <Text style={styles.containerBtnText}>Investimento</Text>
       </TouchableOpacity>
-      <TouchableOpacity  style={styles.containerBtn}>
+      <TouchableOpacity  style={styles.containerBtn} onPress={() => Linking.openURL('https://www.betrybe.com/empresas-parceiras')}>
         <Text style={styles.containerBtnText}>Contrate Trybers</Text>
       </TouchableOpacity>
-      <TouchableOpacity  style={styles.containerBtn}>
+      <TouchableOpacity  style={styles.containerBtn} onPress={() => Linking.openURL('https://blog.betrybe.com/')}>
         <Text style={styles.containerBtnText}>Blog</Text>
       </TouchableOpacity>
-      <TouchableOpacity  style={styles.containerBtn}>
+      <TouchableOpacity  style={styles.containerBtn} onPress={() => Linking.openURL('https://www.betrybe.com/trabalhe-conosco')}>
         <Text style={styles.containerBtnText}>Trabalhe conosco</Text>
       </TouchableOpacity>
-      <TouchableOpacity  style={styles.containerBtn}>
+      <TouchableOpacity  style={styles.containerBtn} onPress={() => Linking.openURL('https://ajuda.betrybe.com/hc/pt-br')}>
         <Text style={styles.containerBtnText}>Dúvidas</Text>
       </TouchableOpacity>
     </View>
