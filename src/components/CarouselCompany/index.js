60febdfa312334b4ec55fa968f5fbe037f4232a9
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-28 00:38:04 -0300

    style: alterando height do container

--- src/components/CarouselCompany/index.js
+++ src/components/CarouselCompany/index.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-    height: 170,
+    height: 250,
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
