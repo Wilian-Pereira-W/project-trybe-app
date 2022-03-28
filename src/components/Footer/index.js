a893cc5dda3491cbfe3e58de37a74532fe185e36
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-28 02:16:45 -0300

    feat: implementando os links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, View, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { Image, View, StyleSheet, Text, TouchableOpacity, Linking} from 'react-native';
 
 function Footer() {
   return (
@@ -9,61 +9,89 @@ function Footer() {
           <Image style={styles.logoImg}
             source={require('../../assets/images/logo.png')}
           />
-          <Image style={styles.certifiedImg}
-            source={{
-              uri: 'https://moodle.com/wp-content/uploads/2021/09/B-Corp_header@2x.png',
-            }}
-          />
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://www.bcorporation.net/pt-br/find-a-b-corp/company/trybe/')}
+          >
+            <Image style={styles.certifiedImg}
+              source={{
+                uri: 'https://moodle.com/wp-content/uploads/2021/09/B-Corp_header@2x.png',
+              }}
+            />
+          </TouchableOpacity>
         </View>
         <View>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://www.betrybe.com/trabalhe-conosco')}
+          >
             <Text style={styles.optionsText}>Tabalhe conosco</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://ajuda.betrybe.com/hc/pt-br')}
+          >
             <Text style={styles.optionsText}>Fale com a gente</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://www.betrybe.com/empresas-parceiras')}
+          >
             <Text style={styles.optionsText}>Quero ser empresa parecira</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://blog.betrybe.com/')}
+          >
             <Text style={styles.optionsText}>Blog</Text>
           </TouchableOpacity>
         </View>
         <View style={{ flexDirection: 'row', marginTop: 50, alignItems: 'center'}}>
-          <TouchableOpacity style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
+          <TouchableOpacity 
+            onPress={() => Linking.openURL('https://www.instagram.com/betrybe/')}
+            style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
             <Image style={{borderRadius: 50}}
               source={require('../../assets/images/instagram.png')}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
+          <TouchableOpacity 
+            onPress={() => Linking.openURL('https://www.facebook.com/betrybe')}
+            style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
             <Image style={{borderRadius: 50}}
               source={require('../../assets/images/facebook.png')}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
+          <TouchableOpacity 
+            onPress={() => Linking.openURL('https://twitter.com/betrybe')}
+            style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
             <Image style={{borderRadius: 50}}
               source={require('../../assets/images/twitter.png')}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
+          <TouchableOpacity 
+            onPress={() => Linking.openURL('https://www.linkedin.com/school/betrybe/')}
+            style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
             <Image style={{borderRadius: 50}}
               source={require('../../assets/images/linkedin.png')}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
+          <TouchableOpacity 
+            onPress={() => Linking.openURL('https://www.youtube.com/channel/UCZeN5eLUwiIyMaQjKR_ydVg')}
+            style={{width: 60, height: 60, margin:10, backgroundColor: '#2FC18C', alignItems: 'center', justifyContent: 'center', borderRadius: 100}}>
             <Image style={{borderRadius: 50}}
               source={require('../../assets/images/youtube.png')}
             />
           </TouchableOpacity>
         </View>
         <View>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://www.betrybe.com/compliance')}
+          >
             <Text style={styles.optionsText1}>Compliance</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://www.betrybe.com/politica-de-privacidade')}
+          >
             <Text style={styles.optionsText1}>Política de Privacidade</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://www.betrybe.com/termos-de-uso')}
+          >
             <Text style={styles.optionsText1}>Termo de Uso</Text>
           </TouchableOpacity>
           <TouchableOpacity>
